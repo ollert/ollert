5a3fc705808d973a62a14ef75af689ac916c2356
Larry Price <larry.price.dev@gmail.com>
2014-06-14 07:52:33 -0400

    calls use .ajax syntax

--- public/js/analyzer.js
+++ public/js/analyzer.js
@@ -7,70 +7,82 @@ var Analyzer = (function() {
   }
 
   var loadWipChart = function(boardId) {
-    $.get("/boards/" + boardId + "/data", function(data) {
-      $('#wip-spinner').hide();
-      var theData = jQuery.parseJSON(data);
-      var wip_data = new wipChartData();
-      wip_data.lists = theData.wipcategories;
-      wip_data.counts = [{
-        name: "Cards in List",
-        showInLegend: false,
-        data: theData.wipdata
-      }];
-      var wc = new wipChart(wip_data);
-      wc.buildChart();
+    $.ajax({
+      url: "/boards/" + boardId + "/data",
+      success: function(data) {
+        $('#wip-spinner').hide();
+        var theData = jQuery.parseJSON(data);
+        var wip_data = new wipChartData();
+        wip_data.lists = theData.wipcategories;
+        wip_data.counts = [{
+          name: "Cards in List",
+          showInLegend: false,
+          data: theData.wipdata
+        }];
+        var wc = new wipChart(wip_data);
+        wc.buildChart();
+      }
     });
   }
 
   var loadCfdChart = function(boardId) {
-    $.get("/boards/" + boardId + "/cfd", function(data) {
-      $('#cfd-spinner').hide();
+    $.ajax({
+      url: "/boards/" + boardId + "/cfd",
+      success: function(data) {
+        $('#cfd-spinner').hide();
 
-      var theData = jQuery.parseJSON(data);
-      var cfdData = new cfdChartData();
-      var cc = new cfdChart({
-        data: theData.cfddata,
-        dates: theData.dates,
-        boardName: "Ollert"
-      });
-      cc.buildChart();
+        var theData = jQuery.parseJSON(data);
+        var cfdData = new cfdChartData();
+        var cc = new cfdChart({
+          data: theData.cfddata,
+          dates: theData.dates,
+          boardName: "Ollert"
+        });
+        cc.buildChart();
+      }
     });
   }
 
   var loadStats = function(boardId) {
-    $.get("/boards/" + boardId + "/stats", function(data) {
-      $.each($('.stats-spinner'), function(i, item) {
-        $(item).hide();
-      });
+    $.ajax({
+      url: "/boards/" + boardId + "/stats",
+      success: function(data) {
+        $.each($('.stats-spinner'), function(i, item) {
+          $(item).hide();
+        });
 
-      var theData = jQuery.parseJSON(data);
-      $('#avg_members_per_card').text(theData.avg_members_per_card);
-      $('#avg_cards_per_member').text(theData.avg_cards_per_member);
-      $('#list_with_most_cards_name').text(theData.list_with_most_cards_name);
-      $('#list_with_most_cards_count').text(theData.list_with_most_cards_count);
-      $('#list_with_least_cards_name').text(theData.list_with_least_cards_name);
-      $('#list_with_least_cards_count').text(theData.list_with_least_cards_count);
-      $('#board_members_count').text(theData.board_members_count);
-      $('#card_count').text(theData.card_count);
-      $('#oldest_card_name').text(theData.oldest_card_name);
-      $('#oldest_card_age').text(theData.oldest_card_age);
-      $('#newest_card_name').text(theData.newest_card_name);
-      $('#newest_card_age').text(theData.newest_card_age);
+        var theData = jQuery.parseJSON(data);
+        $('#avg_members_per_card').text(theData.avg_members_per_card);
+        $('#avg_cards_per_member').text(theData.avg_cards_per_member);
+        $('#list_with_most_cards_name').text(theData.list_with_most_cards_name);
+        $('#list_with_most_cards_count').text(theData.list_with_most_cards_count);
+        $('#list_with_least_cards_name').text(theData.list_with_least_cards_name);
+        $('#list_with_least_cards_count').text(theData.list_with_least_cards_count);
+        $('#board_members_count').text(theData.board_members_count);
+        $('#card_count').text(theData.card_count);
+        $('#oldest_card_name').text(theData.oldest_card_name);
+        $('#oldest_card_age').text(theData.oldest_card_age);
+        $('#newest_card_name').text(theData.newest_card_name);
+        $('#newest_card_age').text(theData.newest_card_age);
+      }
     });
   }
 
   var loadLabelCount = function(boardId) {
-    $.get("/boards/" + boardId + "/labelcounts", function(data) {
-      $('#label-count-spinner').hide();
+    $.ajax({
+      url: "/boards/" + boardId + "/labelcounts",
+      success: function(data) {
+        $('#label-count-spinner').hide();
 
-      var theData = jQuery.parseJSON(data);
+        var theData = jQuery.parseJSON(data);
 
-      var lb_data = new labelCountChartData();
-      lb_data.labels = theData.labels;
-      lb_data.counts = theData.counts;
-      lb_data.colors = theData.colors;
-      var labelCount = new labelCountChart(lb_data);
-      labelCount.buildChart();
+        var lb_data = new labelCountChartData();
+        lb_data.labels = theData.labels;
+        lb_data.counts = theData.counts;
+        lb_data.colors = theData.colors;
+        var labelCount = new labelCountChart(lb_data);
+        labelCount.buildChart();
+      }
     });
   }
 
