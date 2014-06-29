48f485d58fd224d2502a12ad7c271db05ec720d3
Larry Price <larry.price.dev@gmail.com>
2014-06-29 17:20:06 -0400

    fixing bug for viewing private organizations' boards

--- helpers/ollert_helpers.rb
+++ helpers/ollert_helpers.rb
@@ -35,9 +35,13 @@ module OllertHelpers
   
   def get_user_boards(member)
     member.boards.group_by { |board|
-      board.attributes[:organization_id].nil? ?
-          "My Boards"
-          : board.organization.attributes[:display_name]
+      # Check if baord is associated with an organization
+      # If so, make sure member has access to this organization
+      if member.organizations.any? {|org| org.id == board.organization_id}
+        board.organization.display_name
+      else
+        "My Boards"
+      end
     }
   end
   
