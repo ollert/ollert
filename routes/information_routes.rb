dade9c674466420f7e61809c8cf1a84108bbf484
Larry Price <larry.price.dev@gmail.com>
2014-07-21 14:54:10 -0400

    temporary symbol route

--- routes/information_routes.rb
+++ routes/information_routes.rb
@@ -7,6 +7,13 @@ class Ollert
     haml :landing
   end
 
+  # @todo: remove
+  get '/symbols', :auth => :none do
+    data = {symbols: Symbol.all_symbols.count}
+    body data.to_json
+    status 200
+  end
+
   not_found do
     flash[:error] = "The page requested could not be found."
     redirect '/'
