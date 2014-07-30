e77f1b705a8c1af028bb7d63ad6a748e685022f3
Larry Price <larry.price.dev@gmail.com>
2014-07-30 15:47:56 -0400

    removing temporary symbols route

--- routes/information_routes.rb
+++ routes/information_routes.rb
@@ -7,13 +7,6 @@ class Ollert
     haml :landing
   end
 
-  # @todo: remove
-  get '/symbols', :auth => :none do
-    data = {symbols: Symbol.all_symbols.count}
-    body data.to_json
-    status 200
-  end
-
   not_found do
     flash[:error] = "The page requested could not be found."
     redirect '/'
