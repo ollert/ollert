5c5c869bc844389e4b39336223017f4d4ebdeffb
Levi Wilson <levi@leviwilson.com>
2015-02-01 14:09:59 -0500

    move cucumber hooks into hooks.rb

--- test/features/support/env.rb
+++ test/features/support/env.rb
@@ -4,6 +4,7 @@ require_relative '../../../web'
 
 require 'capybara/cucumber'
 require 'rspec'
+require 'dotenv'
 require 'rack_session_access'
 require 'rack_session_access/capybara'
 
@@ -17,20 +18,4 @@ Capybara.javascript_driver = :webkit
 
 World do
   Ollert.new
-end
-
-Before do
-  Mongoid.purge!
-end
-
-After do
-  user = User.find_by(email: ENV['TRELLO_TEST_USERNAME'])
-  if !user.nil? && !user.member_token.nil?
-    client = Trello::Client.new(
-      developer_public_key: ENV['PUBLIC_KEY'],
-      member_token: user.member_token
-    )
-
-    client.delete("/tokens/#{user.member_token}")
-  end
 end
---
+++ test/features/support/hooks.rb
@@ -0,0 +1,15 @@
+Before do
+  Mongoid.purge!
+end
+
+After do
+  user = User.find_by(email: ENV['TRELLO_TEST_USERNAME'])
+  if !user.nil? && !user.member_token.nil?
+    client = Trello::Client.new(
+        developer_public_key: ENV['PUBLIC_KEY'],
+        member_token: user.member_token
+    )
+
+    client.delete("/tokens/#{user.member_token}")
+  end
+end
