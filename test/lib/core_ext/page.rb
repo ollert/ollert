93da4ed1eccaf46d48bb6f3254e7f73a2f28c615
Levi Wilson <levi@leviwilson.com>
2015-02-04 19:25:29 -0500

    add helper to be able to do expect(SomePage).to be_the_current_page

--- test/lib/core_ext/page.rb
+++ test/lib/core_ext/page.rb
@@ -3,5 +3,16 @@ module SitePrism
     def wait_until(&block)
       Waiter.wait_until_true &block
     end
+
+    def self.the_current_page?
+      page = self.new
+
+      begin
+        page.wait_until { page.current_url =~ /#{self.url}$/ }
+      rescue TimeoutException
+        raise "Expected the url to be #{self.url} but it was #{page.current_url}"
+      end
+      true
+    end
   end
 end
