f5327e739bcc90e7da639a1141735b60e97f3677
Levi Wilson <levi@leviwilson.com>
2015-02-07 11:51:13 -0500

    refactor list_action_spec to be units only

--- test/spec/integration/list_action_spec.rb
+++ test/spec/integration/list_action_spec.rb
@@ -2,31 +2,28 @@ require_relative '../spec_helper'
 
 describe Util::ListAction do
   let(:client) { double Trello::Client }
+  let(:right_now) { Time.now }
   let(:_) { anything }
 
   context 'ListAction.all' do
     let(:actions) { Util::ListAction.actions(client, TrelloHelper.board_id, since: right_now.to_s).sort_by(&:date) }
+    let(:response) { [{date: Time.now, data: {card: {}}}].to_json }
 
-    context 'the call' do
-      let(:response) { [{date: Time.now, data: {card: {}}}].to_json }
-      before(:each) { allow(client).to receive(:get).and_return(response) }
+    before(:each) { allow(client).to receive(:get).and_return(response) }
 
-      it 'just wants the actions' do
-        actions
-        expect(client).to have_received(:get).with("/boards/#{TrelloHelper.board_id}/actions", _)
-      end
-
-      it 'can pass additional information' do
-        actions
-        expect(client).to have_received(:get).with(_, hash_including(since: right_now.to_s))
-      end
+    it 'just wants the actions' do
+      actions
+      expect(client).to have_received(:get).with("/boards/#{TrelloHelper.board_id}/actions", _)
     end
 
-    xit 'includes created, moved and closed cards' do
-      @first_card.move_to_list TrelloHelper.second_list
-      @first_card.close!
+    it 'can pass additional information' do
+      actions
+      expect(client).to have_received(:get).with(_, hash_including(since: right_now.to_s))
+    end
 
-      expect(actions.map(&:type)).to eq(['createCard', 'updateCard', 'updateCard'])
+    it 'includes created, moved and closed cards' do
+      actions
+      expect(client).to have_received(:get).with(_, hash_including(filter: 'createCard,updateCard:idList,updateCard:closed'))
     end
   end
 end
