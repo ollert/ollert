23a04ec2842a16db59dac377535df6017528bdd2
Levi Wilson <levi@leviwilson.com>
2015-02-16 17:04:25 -0500

    4 :arrow_right: 2 spaces in js files

--- public/js/list-changes-chart-builder.js
+++ public/js/list-changes-chart-builder.js
@@ -1,83 +1,83 @@
 var ListChangesChartBuilder = (function() {
-    var buildChart = function(options) {
-            $('#list-changes-container').highcharts({
-                chart: {
-                    type: 'column'
-                },
+  var buildChart = function(options) {
+        $('#list-changes-container').highcharts({
+          chart: {
+            type: 'column'
+          },
+          title: {
+            text: 'Time Spent in Lists'
+          },
+          xAxis: {
+            categories: options.categories,
             title: {
-                text: 'Time Spent in Lists'
-            },
-            xAxis: {
-                categories: options.categories,
-            title: {
-                text: null
+              text: null
             }
-            },
-            yAxis: {
-                min: 0,
+          },
+          yAxis: {
+            min: 0,
             title: {
-                text: 'Days'
+              text: 'Days'
             },
             labels: {
-                overflow: 'justify'
+              overflow: 'justify'
             }
-            },
-            legend: {
-                enabled: true
-            },
-            tooltip: {
-                pointFormat: 'Average {series.name}: <strong>{point.y:,.2f}</strong>',
+          },
+          legend: {
+            enabled: true
+          },
+          tooltip: {
+            pointFormat: 'Average {series.name}: <strong>{point.y:,.2f}</strong>',
+            format: '{point.y:,.2f}'
+          },
+          plotOptions: {
+            bar: {
+              dataLabels: {
+                enabled: true,
                 format: '{point.y:,.2f}'
-            },
-            plotOptions: {
-                bar: {
-                    dataLabels: {
-                        enabled: true,
-                        format: '{point.y:,.2f}'
-                    }
-                }
-            },
-            credits: {
-                enabled: false
-            },
-            series: options.data
-            });
-        },
-        load = function(boardId, token) {
-            var container = $("#list-changes-container");
-            container.height(container.height() - 10);
+              }
+            }
+          },
+          credits: {
+            enabled: false
+          },
+          series: options.data
+        });
+      },
+      load = function(boardId, token) {
+        var container = $("#list-changes-container");
+        container.height(container.height() - 10);
 
-            $.ajax({
-                url: "/api/v1/listchanges/" + boardId,
-                data: {
-                    token: token
+        $.ajax({
+          url: "/api/v1/listchanges/" + boardId,
+          data: {
+            token: token
+          },
+          success: function(data) {
+            $('#list-changes-spinner').hide();
+
+            var averages = new Ollert.TimeTracker(jQuery.parseJSON(data)).average();
+            buildChart({
+              categories: averages.lists,
+              data: [
+                {
+                  name: "Total Days",
+                  data: averages.total_days
                 },
-                success: function(data) {
-                    $('#list-changes-spinner').hide();
-
-                    var averages = new Ollert.TimeTracker(jQuery.parseJSON(data)).average();
-                    buildChart({
-                        categories: averages.lists,
-                        data: [
-                    {
-                        name: "Total Days",
-                        data: averages.total_days
-                    },
-                        {
-                            name: "Business Days",
-                        data: averages.business_days
-                        }
-                    ]
-                    });
-                },
-                error: function(xhr) {
-                    $("#list-changes-spinner").hide();
-                    container.text(xhr.responseText);
+                {
+                  name: "Business Days",
+                data: averages.business_days
                 }
+              ]
             });
-        };
+          },
+          error: function(xhr) {
+            $("#list-changes-spinner").hide();
+            container.text(xhr.responseText);
+          }
+        });
+      };
 
-    return {
-        build: load
-    }
+  return {
+    build: load
+  };
 }());
--- public/js/time-tracker.js
+++ public/js/time-tracker.js
@@ -1,80 +1,80 @@
 (function(root) {
-    'use strict';
+  'use strict';
 
-    var average = function(field) {
-            return _.property(field)(this) / this.list_count;
-        },
-        newRunningTotal = function() {
-            return { business_days: 0, total_days: 0, list_count: 0, average: average };
-        },
-        orderTheAveragesBy = function(averages, thisOrder) {
-            if(_.isEmpty(averages.lists)) {
-                return averages;
-            }
+  var average = function(field) {
+        return _.property(field)(this) / this.list_count;
+      },
+      newRunningTotal = function() {
+        return { business_days: 0, total_days: 0, list_count: 0, average: average };
+      },
+      orderTheAveragesBy = function(averages, thisOrder) {
+        if(_.isEmpty(averages.lists)) {
+            return averages;
+        }
 
-            var unordered = _.zip(averages.lists, averages.total_days, averages.business_days),
-                ordered = _.map(thisOrder, function(list) {
-                    var theListMatches = function(set) {
-                        return _.first(set) === list;
-                    };
+        var unordered = _.zip(averages.lists, averages.total_days, averages.business_days),
+            ordered = _.map(thisOrder, function(list) {
+              var theListMatches = function(set) {
+                return _.first(set) === list;
+              };
 
-                    return _.find(unordered, theListMatches);
-                }),
-                orderedWithoutMissing = _.compact(ordered);
+              return _.find(unordered, theListMatches);
+            }),
+            orderedWithoutMissing = _.compact(ordered);
 
             return _.object(['lists', 'total_days', 'business_days'], _.zip.apply(_, orderedWithoutMissing));
+      },
+      countTheDays = function(runningTotals, listTotals) {
+        var listOrDefault = function(id) {
+          return runningTotals[id] = runningTotals[id] || newRunningTotal();
         },
-        countTheDays = function(runningTotals, listTotals) {
-            var listOrDefault = function(id) {
-                    return runningTotals[id] = runningTotals[id] || newRunningTotal();
-                },
-                list = listOrDefault(_.first(listTotals)),
-                times = _.last(listTotals);
+        list = listOrDefault(_.first(listTotals)),
+        times = _.last(listTotals);
 
-            list.total_days += times.total_days;
-            list.business_days += times.business_days;
-            list.list_count++;
-        },
-        aggregateLists = function(actions) {
-            return _.reduce(actions, function(runningTotals, current) {
-                _.each(_.pairs(current), _.partial(countTheDays, runningTotals));
-                return runningTotals;
-            }, {});
+        list.total_days += times.total_days;
+        list.business_days += times.business_days;
+        list.list_count++;
+      },
+      aggregateLists = function(actions) {
+        return _.reduce(actions, function(runningTotals, current) {
+          _.each(_.pairs(current), _.partial(countTheDays, runningTotals));
+          return runningTotals;
+        }, {});
+      };
+
+  function TimeTracker(listChanges) {
+    var lists = listChanges.lists,
+        cardTimes = listChanges.times,
+        listName = function(id) {
+          var found = _.find(lists, function(list) {
+            return list.id === id;
+          });
+
+          return found ? found.name : null;
         };
 
-    function TimeTracker(listChanges) {
-        var lists = listChanges.lists,
-            cardTimes = listChanges.times,
-            listName = function(id) {
-                var found = _.find(lists, function(list) {
-                    return list.id === id;
-                });
+    this.average = function() {
+      var listTotals = _.pairs(aggregateLists(_.pluck(cardTimes, 'times'))),
+          averages;
 
-                return found ? found.name : null;
-            };
+      averages =  _.reduce(listTotals, function(result, kv) {
+        var listId = kv[0],
+            listResult = kv[1],
+            name = listName(listId);
 
-        this.average = function() {
-            var listTotals = _.pairs(aggregateLists(_.pluck(cardTimes, 'times'))),
-                averages;
+        if(_.isString(name)) {
+          result.lists.push(name);
+          result.total_days.push(listResult.average('total_days'));
+          result.business_days.push(listResult.average('business_days'));
+        }
 
-            averages =  _.reduce(listTotals, function(result, kv) {
-                var listId = kv[0],
-                     listResult = kv[1],
-                     name = listName(listId);
+        return result;
+      }, {lists: [], total_days: [], business_days: []});
 
-                if(_.isString(name)) {
-                    result.lists.push(name);
-                    result.total_days.push(listResult.average('total_days'));
-                    result.business_days.push(listResult.average('business_days'));
-                }
-
-                return result;
-            }, {lists: [], total_days: [], business_days: []});
-
-            return orderTheAveragesBy(averages, _.pluck(lists,'name'));
-        };
+      return orderTheAveragesBy(averages, _.pluck(lists,'name'));
     };
+  };
 
-    root.TimeTracker = TimeTracker;
+  root.TimeTracker = TimeTracker;
 })(Ollert);
 
