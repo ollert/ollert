695427306c3d6bed3fbe08935fce9a7cf1a1807e
Larry Price <larry.price.dev@gmail.com>
2015-02-17 09:09:05 -0500

    desparately trying to fix heroku

--- web.rb
+++ web.rb
@@ -26,11 +26,11 @@ class Ollert < Sinatra::Base
 
   configure :production do
     use Rack::SSL
-    use Rack::Rewrite do
-      r301 %r{.*}, 'https://ollertapp.com$&', :if => Proc.new {|rack_env|
-        rack_env['SERVER_NAME'] != 'ollertapp.com'
-      }
-    end
+    # use Rack::Rewrite do
+    #   r301 %r{.*}, 'https://ollertapp.com$&', :if => Proc.new {|rack_env|
+    #     rack_env['SERVER_NAME'] != 'ollertapp.com'
+    #   }
+    # end
   end
 
   set(:auth) do |role|
