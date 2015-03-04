6114157d7816cad0377d6d2511784de816b4bccf
Levi Wilson <levi@leviwilson.com>
2015-03-04 08:54:32 -0500

    have SettingsPage#update_email wait until it is not indicating that it is still saving

--- test/lib/pages/settings_page.rb
+++ test/lib/pages/settings_page.rb
@@ -2,6 +2,11 @@ class SettingsPage < SitePrism::Page
   set_url '/settings'
 
   text_field(:email, 'input#email')
-  button(:update_email, 'input[value="Update Email"]')
   label(:email_status, '#email-status')
+  button(:save_email, 'input[value="Update Email"]')
+
+  def update_email
+    save_email
+    wait_until { email_status != 'Saving...' }
+  end
 end
