5f76c59b83d6b7cb1142091d006b940953587d2d
Larry Price <larry.price.dev@gmail.com>
2015-09-24 08:45:21 -0400

    removing all kinds of stuff from the top nav

--- public/js/ollert.js
+++ public/js/ollert.js
@@ -28,52 +28,20 @@ var Ollert = (function() {
   };
 
   var loadBoardsCallback = function(data) {
-    // loadSimpleBoards('Work in progress');
-    var boardData = data['data'];
-    var menus = $('.my-boards');
-
-    console.log(data)
-    var boardItem;
-    for (var boardName in boardData) {
-      var organization = boardData[boardName],
-          organizationBoards = $("<ul/>");
+    var boardData = data['data'], boardItem, board, organization, boards;
+    for (var orgName in boardData) {
+      organization = boardData[orgName];
+      organizationBoards = $("<ul/>");
       for (var j = 0; j < organization.length; ++j) {
-        var board = organization[j]
+        board = organization[j];
         item = $("<li/>", {
           role: "presentation"
         });
         item.append($("<a href=\"/boards/" + board.id + "\">" + board.name + "</a>"));
         organizationBoards.append(item);
       }
-      var section = $("<li role=\"presentation\"><b>" + boardName + "</b></li>").append(organizationBoards)
+      var section = $("<li role=\"presentation\"><b>" + orgName + "</b></li>").append(organizationBoards)
       $("#config-drawer-board-list").append(section);
-    }
-
-
-    for (var organization in boardData) {
-      if (boardData.hasOwnProperty(organization)) {
-        menus.each(function() {
-          var element = $("<li>" + organization + "</li>");
-          element.addClass('divider');
-          element.addClass('divider-section');
-
-          $(this).append(element);
-        });
-
-        for (var i = 0; i < boardData[organization].length; i++) {
-          var board = boardData[organization][i];
-          menus.each(function() {
-            var element = $("<li></li>");
-            var link = $("<a>" + board['name'] + "</a>");
-            link.addClass('btn');
-            link.addClass('btn-link');
-            link.attr('href', '/boards/' + board['id']);
-
-            element.append(link);
-            $(this).append(element);
-          });
-        }
-      }
     }
   };
 
