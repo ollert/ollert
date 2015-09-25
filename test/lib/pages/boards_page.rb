f5282288a9c3a977bc105fd665030f8576e45605
Larry Price <larry.price.dev@gmail.com>
2015-09-25 15:18:53 -0400

    fixing cukes using within context

--- test/lib/pages/boards_page.rb
+++ test/lib/pages/boards_page.rb
@@ -2,13 +2,12 @@ class BoardsPage < SitePrism::Page
   set_url '/boards'
 
   element :drawer_controls, ".drawer-controls a"
-  element :board_list_first_item, "#config-drawer-board-list li:first-child"
-
-  # alias_method :choose, :click_on
 
   def choose(board_name)
-    # wait_for_board_list_first_item 10
-    # drawer_controls.click
-    click_on board_name
+    drawer_controls.click
+    wait_for_ajax
+    within '.drawer-contents' do
+      click_on board_name
+    end
   end
 end
