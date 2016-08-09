62933cf3ebf9b26d895338b04d305001b778398a
Levi Wilson <levi@leviwilson.com>
2016-08-09 00:10:17 -0400

    DRY up the usage of Trello::Client.new

--- routes/analysis_routes.rb
+++ routes/analysis_routes.rb
@@ -9,25 +9,21 @@ class Ollert
       if params["token"].nil?
         halt 400, "Missing token."
       end
+
+      @client = Trello::Client.new(developer_public_key: ENV['PUBLIC_KEY'],
+                                   member_token: params['token'])
     end
   end
 
   get '/api/v1/progress/:board_id' do |board_id|
-    client = Trello::Client.new(
-      :developer_public_key => ENV['PUBLIC_KEY'],
-      :member_token => params["token"]
-    )
-
-    body ProgressChartsAnalyzer.analyze(ProgressChartsFetcher.fetch(client, board_id),
+    body ProgressChartsAnalyzer.analyze(ProgressChartsFetcher.fetch(@client, board_id),
      params["startingList"], params["endingList"]).to_json
     status 200
   end
 
   get '/api/v1/listchanges/:board_id' do |board_id|
-    client = Trello::Client.new developer_public_key: ENV['PUBLIC_KEY'], member_token: params['token']
-
-    lists = client.get("/boards/#{board_id}/lists", filter: 'open').json_into(Trello::List)
-    all = Utils::Fetchers::ListActionFetcher.fetch(client, board_id)
+    lists = @client.get("/boards/#{board_id}/lists", filter: 'open').json_into(Trello::List)
+    all = Utils::Fetchers::ListActionFetcher.fetch(@client, board_id)
 
     {
       lists: lists.map {|l| {id: l.id, name: l.name}},
@@ -36,32 +32,17 @@ class Ollert
   end
 
   get '/api/v1/wip/:board_id' do |board_id|
-    client = Trello::Client.new(
-      :developer_public_key => ENV['PUBLIC_KEY'],
-      :member_token => params["token"]
-    )
-
-    body WipAnalyzer.analyze(WipFetcher.fetch(client, board_id)).to_json
+    body WipAnalyzer.analyze(WipFetcher.fetch(@client, board_id)).to_json
     status 200
   end
 
   get '/api/v1/stats/:board_id' do |board_id|
-    client = Trello::Client.new(
-      :developer_public_key => ENV['PUBLIC_KEY'],
-      :member_token => params['token']
-    )
-
-    body StatsAnalyzer.analyze(StatsFetcher.fetch(client, board_id)).to_json
+    body StatsAnalyzer.analyze(StatsFetcher.fetch(@client, board_id)).to_json
     status 200
   end
 
   get '/api/v1/labels/:board_id' do |board_id|
-    client = Trello::Client.new(
-      :developer_public_key => ENV['PUBLIC_KEY'],
-      :member_token => params['token']
-    )
-
-    body LabelCountAnalyzer.analyze(LabelCountFetcher.fetch(client, board_id)).to_json
+    body LabelCountAnalyzer.analyze(LabelCountFetcher.fetch(@client, board_id)).to_json
     status 200
   end
 end
