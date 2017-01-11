fa52816cdd01b6d5c8fa0f41b81aec526dd75877
Larry Price <larry.price.dev@gmail.com>
2017-01-10 20:20:10 -0500

    minor semantics update

--- utils/analyzers/stats_analyzer.rb
+++ utils/analyzers/stats_analyzer.rb
@@ -98,7 +98,7 @@ class StatsAnalyzer
   end
 
   def self.get_newest_card(cards, actions)
-    oldest = actions.max_by { |action| action["date"].to_date }
-    return oldest["data"]["card"]["name"], DateTime.now.utc.mjd - oldest["date"].to_datetime.utc.mjd unless oldest.nil?
+    newest = actions.max_by { |action| action["date"].to_date }
+    return newest["data"]["card"]["name"], DateTime.now.utc.mjd - newest["date"].to_datetime.utc.mjd unless oldest.nil?
   end
 end
